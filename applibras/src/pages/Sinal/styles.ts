72ffcce7c63db9e1ed2f0730876188feea8e699b
lidialopes <lidiajlopes@gmail.com>
2020-08-04 16:37:57 -0300

    define needed styled-components

--- applibras/src/pages/Sinal/styles.ts
+++ applibras/src/pages/Sinal/styles.ts
@@ -1,2 +1,26 @@
 import styled from 'styled-components/native';
 
+export const Title = styled.Text`
+  margin: 20px 0 5px;
+  font-family: 'OpenSans-SemiBold';
+  font-size: 20px;
+  color: #212121;
+`;
+
+export const SignWriting = styled.Image`
+  border-radius: 6px;
+  height: 300px;
+  margin: 5px;
+`;
+
+export const Description = styled.Text`
+  margin: 0 0 10px;
+  font-family: 'OpenSans';
+  font-size: 15px;
+  color: #212121;
+  text-align: justify;
+`;
+
+export const SignInfo = styled.ScrollView`
+  margin-bottom: 15px;
+`;
