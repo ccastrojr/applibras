b5c064c4cd0a1a8054ad651a776c44864153264e
lidialopes <lidiajlopes@gmail.com>
2020-08-04 16:50:16 -0300

    build Sinal page layout

--- applibras/src/pages/Sinal/index.tsx
+++ applibras/src/pages/Sinal/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
+import YoutubePlayer from 'react-native-youtube-iframe';
 import { useRoute } from '@react-navigation/native';
 
-import { Container } from './../Dashboard/styles';
+import { 
+  Title,
+  SignWriting,
+  Description,
+  SignInfo,
+} from './styles';
+
+import { 
+  Container,
+  VideoTitle,
+  TagsWrapper,
+  Tag 
+} from './../Dashboard/styles'
+
+import Button from './../../components/Button';
 
 interface Params {
   title: string;
@@ -15,7 +31,34 @@ const Sinal: React.FC = () => {
   const route = useRoute();
   const routeParams = route.params as Params;
 
-  return <Container />;
+  return (
+    <Container>
+      <SignInfo>
+        <YoutubePlayer
+          height={(Dimensions.get('window').width - 60) * 0.5625}
+          width={Dimensions.get('window').width - 60}
+          videoId={routeParams.videoURL}
+          initialPlayerParams={{}}
+        />
+        <VideoTitle>{routeParams.title}</VideoTitle>
+        <TagsWrapper>
+          {routeParams.tags.map(tag => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </TagsWrapper>
+
+        <Title>Escrita do Sinal</Title>
+        <SignWriting
+          source={{uri: routeParams.imageURL}}
+        />
+        
+        <Title>Descrição</Title>
+        <Description>{routeParams.description}</Description>
+      </SignInfo>
+
+      <Button>Sugerir Correção</Button>
+    </Container>
+  );
 };
 
 export default Sinal;
