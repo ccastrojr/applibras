45e20915bd942c6418e96b3563e0e3a79c761972
lidialopes <lidiajlopes@gmail.com>
2020-08-04 16:43:33 -0300

    define interface Params

--- applibras/src/pages/Sinal/index.tsx
+++ applibras/src/pages/Sinal/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useRoute } from '@react-navigation/native';
 
 import { Container } from './../Dashboard/styles';
 
+interface Params {
+  title: string;
+  videoURL: string;
+  imageURL: string,
+  description: string;
+  tags: string[];
+}
+
 const Sinal: React.FC = () => {
+  const route = useRoute();
+  const routeParams = route.params as Params;
+
   return <Container />;
 };
 
